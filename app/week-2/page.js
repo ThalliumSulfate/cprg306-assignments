e979c1bb4b967ae3bd4a4a7190b4301b9273c1c9
thalliumsulfate <thalia.munroe@edu.sait.ca>
2024-09-11 19:07:30 -0600

    week 2 added

---
+++ app/week-2/page.js
@@ -0,0 +1,10 @@
+import Student_info from "./student_info";
+
+export default function Page() {
+    return (
+        <main>
+                <h1>Shopping List</h1>
+                <Student_info/>
+        </main>
+    )
+}
---
+++ app/week-2/student_info.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+function student_info() {
+    return (
+        <>
+            <p>Name: Thalia Munroe</p>
+            <p>
+                Github: <Link href="https://github.com/ThalliumSulfate" >github.com/ThalliumSulfate</Link>
+            </p>
+        </>
+    )
+}
+
+export default student_info;
